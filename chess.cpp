dd113c26babcf74f2574fdc5a7d7df94c9db6be6
ryanvanderstelt <ryan@vstelt.com>
2024-09-26 01:40:39 -0500

    scrapped board struct in favor of just a list of Pieces. Made printed chess board look pretty

--- chess.cpp
+++ chess.cpp
@@ -3,17 +3,170 @@
 
 using namespace std;
 
+// devins bit idea for castle memory (maybe)
 
-//how to store info (class/struct)
-//
-//how to iterate through pieces (list, array, etc)
-//maybe just array with coords math (smart, simple)
-//
-//functions are trivial, just need to know how to manage data
-//
-//devins bit idea for castle memory (maybe)
+/*
 
-int main() {
-    Board board;
-    board.printBoard();
+Game Plan:
+
+1. Initialize game
+2. User input for piece
+    a. Check if location given holds one of the active players pieces.
+    b. Translate from chess jargon to zero-indexed column and row (e8 == 4, 7)
+3. List moves for piece
+    a. Check if in bounds
+    b. Check if lands on ally
+    b. Check if active player will be in check
+    c. Display options
+4. User input for destination
+    a. Any invalid move unfocuses piece (go back to step 2)
+5. Move piece to destination
+6. Evaluate board
+    a. If enemy is in check, check for checkmate
+        1. If checkmate, end game
+        2. If not checkmate, alert of check
+7. Swap player and go back to step 2
+
+*/
+
+void initializeBoard(Piece *&board);
+
+void printBoard(Piece *&board);
+
+int main()
+{
+    Piece *board;
+    bool not_checkmate = true;
+
+    initializeBoard(board);
+    printBoard(board);
+    // while (not_checkmate) {
+
+    // }
+}
+
+void initializeBoard(Piece *&board)
+{
+    board = new Piece[64];
+    for (int i = 0; i < 16; i++)
+    {
+        switch (i)
+        {
+        case 0:
+        case 7:
+            board[i] = Piece('r', i);
+            board[56 + i] = Piece('R', 56 + i);
+            break;
+
+        case 1:
+        case 6:
+            board[i] = Piece('n', i);
+            board[56 + i] = Piece('N', 56 + i);
+            break;
+
+        case 2:
+        case 5:
+            board[i] = Piece('b', i);
+            board[56 + i] = Piece('B', 56 + i);
+            break;
+
+        case 3:
+            board[i] = Piece('q', i);
+            board[56 + i] = Piece('Q', 56 + i);
+            break;
+
+        case 4:
+            board[i] = Piece('k', i);
+            board[56 + i] = Piece('K', 56 + i);
+            break;
+
+        default:
+            board[i] = Piece('p', i);
+            board[63 - i] = Piece('P', 63 - i);
+            break;
+        }
+    }
+}
+
+void printBoard(Piece *&board)
+{
+    string rook[2] = {"\u2656", "\u265C"};
+    string knight[2] = {"\u2658", "\u265E"};
+    string bishop[2] = {"\u2657", "\u265D"};
+    string queen[2] = {"\u2655", "\u265B"};
+    string king[2] = {"\u2654", "\u265A"};
+    string pawn[2] = {"\u2659", "\u265F"};
+    bool white_square;
+
+    for (int i = 0; i < 64; i++)
+    {
+        white_square = (i + i / 8) % 2 == 0;
+        if (i % 8 == 0)
+        {
+            cout << endl;
+        }
+        if (white_square)
+        {
+            cout << "\033[30;47m";
+        }
+        else
+        {
+            cout << "\033[37;40m";
+        }
+        switch (board[i].type)
+        {
+        case 'r':
+            cout << ' ' << rook[white_square] << ' ';
+            break;
+
+        case 'n':
+            cout << ' ' << knight[white_square] << ' ';
+            break;
+
+        case 'b':
+            cout << ' ' << bishop[white_square] << ' ';
+            break;
+
+        case 'q':
+            cout << ' ' << queen[white_square] << ' ';
+            break;
+
+        case 'k':
+            cout << ' ' << king[white_square] << ' ';
+            break;
+
+        case 'p':
+            cout << ' ' << pawn[white_square] << ' ';
+            break;
+
+        case 'R':
+            cout << ' ' << rook[!white_square] << ' ';
+            break;
+
+        case 'N':
+            cout << ' ' << knight[!white_square] << ' ';
+            break;
+
+        case 'B':
+            cout << ' ' << bishop[!white_square] << ' ';
+            break;
+
+        case 'Q':
+            cout << ' ' << queen[!white_square] << ' ';
+            break;
+
+        case 'K':
+            cout << ' ' << king[!white_square] << ' ';
+            break;
+
+        case 'P':
+            cout << ' ' << pawn[!white_square] << ' ';
+            break;
+
+        default:
+            cout << "   ";
+        }
+    }
+    cout << "\033[0m" << endl
+         << endl;
 }
--- piece_board.h
+++ piece_board.h
@@ -2,87 +2,31 @@
 
 using namespace std;
 
-// struct Move {
-//     int moveFrom;
-//     int moveTo;
-// };
-
-class Piece {
-    public:
-        char type;
-        int col;
-        int row;
-        bool moved = false;
-        bool passantable = false;
-
-        Piece() {
-        };
-        Piece(char piece_type, int location) {
-            type = piece_type;
-            col = location % 8;
-            row = location / 8;
-        }
-        // Move* listMoves() {
-        //     return;
-        // }
+struct Move
+{
+    int moveFrom;
+    int moveTo;
 };
 
-// struct Player {
-//     Piece pieces[16];
-//     Move moveList[];
-// };
+struct Piece
+{
+public:
+    char type;
+    int col;
+    int row;
+    bool moved = false;
+    bool passantable = false;
 
-class Board {
-    public:
-        Piece* board;
-        Board() {
-            board = new Piece[64];
-            for (int i = 0; i < 16; i++) {
-                switch (i)
-                {
-                case 0: case 7:
-                    board[i] = Piece('r', i);
-                    board[56 + i] = Piece('R', 56 + i);
-                    break;
-
-                case 1: case 6:
-                    board[i] = Piece('n', i);
-                    board[56 + i] = Piece('N', 56 + i);
-                    break;
-
-                case 2: case 5:
-                    board[i] = Piece('b', i);
-                    board[56 + i] = Piece('B', 56 + i);
-                    break;
-
-                case 3:
-                    board[i] = Piece('q', i);
-                    board[56 + i] = Piece('Q', 56 + i);
-                    break;
-                
-                case 4:
-                    board[i] = Piece('k', i);
-                    board[56 + i] = Piece('K', 56 + i);
-                    break;
-
-                default:
-                    board[i] = Piece('p', i);
-                    board[63 - i] = Piece('P', 63 - i);
-                    break;
-                }
-            }
-        }
-        void printBoard() {
-            for (int i=0; i<64; i++) {
-                if (i % 8 == 0) {
-                    cout << endl;
-                }
-                if (board[i].type) {
-                    cout << board[i].type;
-                } else {
-                    cout << ' ';
-                }
-            }
-            cout << endl;    
-        }
+    Piece() {
+    };
+    Piece(char piece_type, int location)
+    {
+        type = piece_type;
+        col = location % 8;
+        row = location / 8;
+    }
+    void move(Move move, Piece *board)
+    {
+        board[move.moveTo] = board[move.moveFrom];
+    }
 };
